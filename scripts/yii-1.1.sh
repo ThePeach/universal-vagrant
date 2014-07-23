68376a885b2ff53487bad29284b8c0e3f868ee7e
Matteo Pescarin <peach@smartart.it>
2014-07-23 16:53:27 +0100

    updated yii to version 1.1.15 (security fix)

--- scripts/yii-1.1.sh
+++ scripts/yii-1.1.sh
@@ -3,7 +3,7 @@
 echo -e "\n--- Installing Yii 1.1 ---\n"
 cd /
 rm -rf yii*
-wget -q https://github.com/yiisoft/yii/releases/download/1.1.14/yii-1.1.14.f0fee9.tar.gz
-tar -xpf yii-1.1.14.f0fee9.tar.gz
-rm yii-1.1.14.f0fee9.tar.gz
+wget -q https://github.com/yiisoft/yii/releases/download/1.1.15/yii-1.1.15.022a51.tar.gz 
+tar -xpf yii-1.1.15.022a51.tar.gz
+rm yii-1.1.15.022a51.tar.gz
 
